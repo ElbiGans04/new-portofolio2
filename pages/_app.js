b4d061ab0c11e435972cc984e4bc636c0cc63823
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2023-01-24 17:01:53 +0700

    slicing halaman home

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,17 @@ import { store } from "@/src/store";
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
+import {IconContext } from 'react-icons'
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
+        <IconContext.Provider  value={{
+          style: { verticalAlign: 'middle' }
+        }}>
+          <Component {...pageProps} />
+        </IconContext.Provider>
       </ChakraProvider>
     </Provider>
   );

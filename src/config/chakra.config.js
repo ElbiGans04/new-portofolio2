7a36a5eaa2bddfd72c0c214ed972e7c05768ecb8
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2023-01-30 12:45:01 +0700

    Setting custom breakpoints chakra ui

--- src/config/chakra.config.js
+++ src/config/chakra.config.js
@@ -1,7 +1,23 @@
-import { extendTheme  } from '@chakra-ui/react'
-import source_sans from '../constants/fonts/source_sans_3';
-import work_sans from '../constants/fonts/work_sans';
+import { extendTheme } from "@chakra-ui/react";
+import source_sans from "../constants/fonts/source_sans_3";
+import work_sans from "../constants/fonts/work_sans";
+
+/**
+ * Custom Breakpoint
+ */
+const breakpoints = {
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1280px",
+  "2xl": "1536px",
+};
+
+/**
+ * Custom Configuration
+ */
 const configuration = {
+  breakpoints,
   colors: {
     brand: {
       1000: "#0b001a",
@@ -17,57 +33,56 @@ const configuration = {
       50: "#8424FF",
     },
   },
-   fonts: {
+  fonts: {
     body: source_sans.style.fontFamily,
     heading: work_sans.style.fontFamily,
   },
-  components : {
-    Switch : {
-      variants : {
-        brand : {
-          backgroundColor: 'brand.50'
-        }
+  components: {
+    Switch: {
+      variants: {
+        brand: {
+          backgroundColor: "brand.50",
+        },
       },
       defaultProps: {
-        colorScheme: 'brand',
+        colorScheme: "brand",
       },
     },
-    Button : {
-      variants : {
-        brand : {
-          backgroundColor: 'brand.50',
-          cursor: 'pointer',
-          color: 'white',
-          padding: '10px',
+    Button: {
+      variants: {
+        brand: {
+          backgroundColor: "brand.50",
+          cursor: "pointer",
+          color: "white",
+          padding: "10px",
           _hover: {
-            backgroundColor: 'brand.600',
-            color: 'whiteAlpha.900'
+            backgroundColor: "brand.600",
+            color: "whiteAlpha.900",
           },
           _active: {
-            backgroundColor: 'brand.700',
-            color: 'whiteAlpha.800'
+            backgroundColor: "brand.700",
+            color: "whiteAlpha.800",
           },
         },
-        brandOutline : {
-          backgroundColor: 'white',
-          color: 'brand.50',
-          cursor: 'pointer',
-          borderWidth: '1px',
-          borderColor: 'brand.50',
-          padding: '10px',
+        brandOutline: {
+          backgroundColor: "white",
+          color: "brand.50",
+          cursor: "pointer",
+          borderWidth: "1px",
+          borderColor: "brand.50",
+          padding: "10px",
           _hover: {
-            backgroundColor: 'gray.100',
-            color: 'brand.500'
+            backgroundColor: "gray.100",
+            color: "brand.500",
           },
           _active: {
-            backgroundColor: 'gray.200',
-            color: 'brand.600'
+            backgroundColor: "gray.200",
+            color: "brand.600",
           },
-        }
+        },
       },
-    }
-  }
+    },
+  },
 };
-
 
 export default extendTheme(configuration);

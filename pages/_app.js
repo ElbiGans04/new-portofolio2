6092ec820fb6d52aa6689b915c2d81a8eaf47315
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2023-02-08 16:35:11 +0700

    Menghapus package, reducer dan store redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,24 @@
 import theme from "@/src/config/chakra.config";
-import { store } from "@/src/store";
 import { ChakraProvider, Container } from "@chakra-ui/react";
-import { Provider } from "react-redux";
-import {IconContext } from 'react-icons'
+import { IconContext } from "react-icons";
 import Navbar from "@/src/components/Navbar";
 import Footer from "@/src/components/Footer";
 import style from "@/src/constants/styles";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <ChakraProvider theme={theme}>
-        <IconContext.Provider  value={{
-          style: { verticalAlign: 'middle' }
-        }}>
-          <Navbar />
-          <Container maxW={style.maxWidthContent}>
-            <Component {...pageProps} />
-          </Container>
-          <Footer />
-        </IconContext.Provider>
-      </ChakraProvider>
-    </Provider>
+    <ChakraProvider theme={theme}>
+      <IconContext.Provider
+        value={{
+          style: { verticalAlign: "middle" },
+        }}
+      >
+        <Navbar />
+        <Container maxW={style.maxWidthContent}>
+          <Component {...pageProps} />
+        </Container>
+        <Footer />
+      </IconContext.Provider>
+    </ChakraProvider>
   );
 }

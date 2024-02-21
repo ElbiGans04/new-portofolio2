1dc55a6f955f76144591cc877e6a9d43f8d0d9b1
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2024-02-21 17:46:14 +0700

    improve: membuat progress loading saat berpindah halaman

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -18,6 +18,8 @@ import {
   Button,
   forwardRef,
   useColorMode,
+  Progress,
+  VStack,
 } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
@@ -25,6 +27,8 @@ import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { BsFillMoonFill, BsSunFill } from "react-icons/bs";
 import { breakpoints } from "@/src/config/chakra.config";
 import urls from "@/src/constants/url";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const ButtonCustomMenu = forwardRef(({ isActive, ...props }, ref) => (
   <Button
@@ -43,6 +47,48 @@ const ButtonCustomMenu = forwardRef(({ isActive, ...props }, ref) => (
   </Button>
 ));
 
+function ProgressBar() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const [enable, setEnable] = useState({
+    show: false,
+    url: '',
+  });
+
+  // For Enable
+  useEffect(() => {
+    if (enable.url != pathname) {
+      setEnable({
+        show: true,
+        url: `${pathname}`
+      });
+    }
+  }, [pathname, setEnable, enable]);
+
+  // For Disabled
+  useEffect(() => {
+    if (enable.show) {
+      setTimeout(() => {
+        setEnable(state => ({
+          ...state,
+          show: false
+        }));
+      }, 500)
+    }
+  }, [enable, setEnable])
+
+  return (
+    <Progress
+      w="100%"
+      h="10px"
+      size="md"
+      colorScheme={"brand"}
+      isIndeterminate={enable.show}
+      backgroundColor="transparent"
+    ></Progress>
+  );
+}
+
 export default function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
   const router = useRouter();
@@ -52,144 +98,145 @@ export default function Navbar() {
   });
 
   return (
-    <HStack
-      w={["100%"]}
-      h={["100%"]}
-      justifyContent={["center"]}
-      position={["sticky"]}
-      top={["0"]}
-      left={["0"]}
-      right="0"
-      bottom="0"
-      backgroundColor="bgLayer1" color="textLayer1"
-      borderBottomColor={["borderLayer1"]}
-      borderBottomWidth={["1px"]}
-      zIndex={style.navbarZIndex}
-      paddingY={["16px"]}
-      paddingX={[0, null, null, null, "48px", "0"]}
-    >
-      <Container maxW={style.maxWidthContent}>
-        <Flex justifyContent="space-between" align={["center"]}>
-          <Link
-            color="brand.50"
-            fontSize={["3xl", "5xl"]}
-            fontWeight={["bold"]}
-            href="/"
-            as={NextLink}
-          >
-            Elbi
-          </Link>
-          {isHideMenu ? (
-            <Stack
-              direction={["row"]}
-              spacing={["32px"]}
-              alignItems={["center"]}
+    <VStack backgroundColor="bgLayer1" zIndex={9999} position={["sticky"]} top={["0"]} left={["0"]} right="0" bottom="0">
+      <ProgressBar></ProgressBar>
+      <HStack
+        w={["100%"]}
+        h={["100%"]}
+        justifyContent={["center"]}
+        color="textLayer1"
+        borderBottomColor={["borderLayer1"]}
+        borderBottomWidth={["1px"]}
+        zIndex={style.navbarZIndex}
+        paddingY={["16px"]}
+        paddingX={[0, null, null, null, "48px", "0"]}
+        marginTop={`0!important`}
+      >
+        <Container maxW={style.maxWidthContent}>
+          <Flex justifyContent="space-between" align={["center"]}>
+            <Link
+              color="brand.50"
+              fontSize={["3xl", "5xl"]}
+              fontWeight={["bold"]}
+              href="/"
+              as={NextLink}
             >
-              <Link
-                as={NextLink}
-                href="/"
-                fontWeight={["semibold"]}
-                color={router.pathname === "/" ? "brand.50" : "textLayer1"}
-                fontSize={["lg", "2xl"]}
-              >
-                Home
-              </Link>
-              <Link
-                as={NextLink}
-                href={urls.blogs.url}
-                fontWeight={["semibold"]}
-                color={
-                  router.pathname === urls.blogs.url ? "brand.50" : "textLayer1"
-                }
-                fontSize={["lg", "2xl"]}
-              >
-                Blog
-              </Link>
-              <HStack
+              Elbi
+            </Link>
+            {isHideMenu ? (
+              <Stack
+                direction={["row"]}
+                spacing={["32px"]}
                 alignItems={["center"]}
-                justifyContent={["space-between"]}
-                spacing={["10px"]}
               >
-                <BsSunFill />
-                <Switch
-                  isChecked={colorMode !== "light"}
-                  variant={"brand"}
-                  onChange={toggleColorMode}
-                />
-                <BsFillMoonFill />
-              </HStack>
-            </Stack>
-          ) : (
-            <Menu closeOnSelect={false}>
-              {({ isOpen }) => (
-                <>
-                  <MenuButton isActive={isOpen} as={ButtonCustomMenu} />
-                  <MenuList>
-                    <MenuGroup title="Page">
-                      <MenuItem
-                        as={NextLink}
-                        href="/"
-                        _hover={{
-                          backgroundColor: ["brand.100"],
-                        }}
-                        _active={{
-                          backgroundColor: ["brand.200"],
-                        }}
+                <Link
+                  as={NextLink}
+                  href="/"
+                  fontWeight={["semibold"]}
+                  color={router.pathname === "/" ? "brand.50" : "textLayer1"}
+                  fontSize={["lg", "2xl"]}
+                >
+                  Home
+                </Link>
+                <Link
+                  as={NextLink}
+                  href={urls.blogs.url}
+                  fontWeight={["semibold"]}
+                  color={
+                    router.pathname === urls.blogs.url
+                      ? "brand.50"
+                      : "textLayer1"
+                  }
+                  fontSize={["lg", "2xl"]}
+                >
+                  Blog
+                </Link>
+                <HStack
+                  alignItems={["center"]}
+                  justifyContent={["space-between"]}
+                  spacing={["10px"]}
+                >
+                  <BsSunFill />
+                  <Switch
+                    isChecked={colorMode !== "light"}
+                    variant={"brand"}
+                    onChange={toggleColorMode}
+                  />
+                  <BsFillMoonFill />
+                </HStack>
+              </Stack>
+            ) : (
+              <Menu closeOnSelect={false}>
+                {({ isOpen }) => (
+                  <>
+                    <MenuButton isActive={isOpen} as={ButtonCustomMenu} />
+                    <MenuList>
+                      <MenuGroup title="Page">
+                        <MenuItem
+                          as={NextLink}
+                          href="/"
+                          _hover={{
+                            backgroundColor: ["brand.100"],
+                          }}
+                          _active={{
+                            backgroundColor: ["brand.200"],
+                          }}
+                        >
+                          Home
+                        </MenuItem>
+                        <MenuItem
+                          as={NextLink}
+                          href={urls.blogs.url}
+                          _hover={{
+                            backgroundColor: ["brand.100"],
+                          }}
+                          _active={{
+                            backgroundColor: ["brand.200"],
+                          }}
+                        >
+                          Blog
+                        </MenuItem>
+                      </MenuGroup>
+                      <MenuDivider />
+                      <MenuOptionGroup
+                        onChange={toggleColorMode}
+                        value={colorMode}
+                        title="Mode"
+                        type="radio"
                       >
-                        Home
-                      </MenuItem>
-                      <MenuItem
-                        as={NextLink}
-                        href={urls.blogs.url}
-                        _hover={{
-                          backgroundColor: ["brand.100"],
-                        }}
-                        _active={{
-                          backgroundColor: ["brand.200"],
-                        }}
-                      >
-                        Blog
-                      </MenuItem>
-                    </MenuGroup>
-                    <MenuDivider />
-                    <MenuOptionGroup
-                      onChange={toggleColorMode}
-                      value={colorMode}
-                      title="Mode"
-                      type="radio"
-                    >
-                      <MenuItemOption
-                        _hover={{
-                          backgroundColor: ["brand.100"],
-                        }}
-                        _active={{
-                          backgroundColor: ["brand.200"],
-                        }}
-                        value="light"
-                        icon={<BsSunFill />}
-                      >
-                        Light Mode
-                      </MenuItemOption>
-                      <MenuItemOption
-                        _hover={{
-                          backgroundColor: ["brand.100"],
-                        }}
-                        _active={{
-                          backgroundColor: ["brand.200"],
-                        }}
-                        value="dark"
-                        icon={<BsFillMoonFill />}
-                      >
-                        Dark Mode
-                      </MenuItemOption>
-                    </MenuOptionGroup>
-                  </MenuList>
-                </>
-              )}
-            </Menu>
-          )}
-        </Flex>
-      </Container>
-    </HStack>
+                        <MenuItemOption
+                          _hover={{
+                            backgroundColor: ["brand.100"],
+                          }}
+                          _active={{
+                            backgroundColor: ["brand.200"],
+                          }}
+                          value="light"
+                          icon={<BsSunFill />}
+                        >
+                          Light Mode
+                        </MenuItemOption>
+                        <MenuItemOption
+                          _hover={{
+                            backgroundColor: ["brand.100"],
+                          }}
+                          _active={{
+                            backgroundColor: ["brand.200"],
+                          }}
+                          value="dark"
+                          icon={<BsFillMoonFill />}
+                        >
+                          Dark Mode
+                        </MenuItemOption>
+                      </MenuOptionGroup>
+                    </MenuList>
+                  </>
+                )}
+              </Menu>
+            )}
+          </Flex>
+        </Container>
+      </HStack>
+    </VStack>
   );
 }
